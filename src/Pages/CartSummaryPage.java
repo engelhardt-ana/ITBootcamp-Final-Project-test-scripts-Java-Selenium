677955caab77084e8b57b00d52959dd0e9b9bcdf
engelhardt-ana <engelhardt.ana98@gmail.com>
2021-06-21 13:07:09 +0200

    Complete Cart Summary Page and Meal Page

--- src/Pages/CartSummaryPage.java
+++ src/Pages/CartSummaryPage.java
@@ -1,5 +1,24 @@
 package Pages;
 
-public class CartSummaryPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class CartSummaryPage extends BasicPage {
+
+	public CartSummaryPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
+		super(driver, waiter, js);
+
+	}
+
+	public WebElement getClearAllCart() {
+		return this.driver.findElement(By.xpath("//*[@class='cart-head']/a[2]"));
+	}
+
+	public void clearAllCart() {
+		this.getClearAllCart().click();
+	}
 
 }
--- src/Pages/MealPage.java
+++ src/Pages/MealPage.java
@@ -1,5 +1,47 @@
 package Pages;
 
-public class MealPage {
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MealPage extends BasicPage {
+
+	public MealPage(WebDriver driver, WebDriverWait waiter, JavascriptExecutor js) {
+		super(driver, waiter, js);
+
+	}
+
+	public WebElement getFavorite() {
+		return this.driver.findElement(By.id("item_95"));
+	}
+
+	public void favorite() {
+		this.getFavorite().click();
+	}
+
+	public WebElement getAddToCartBtn() {
+		return this.driver.findElement(By.className("js-proceedtoAddInCart"));
+	}
+
+	public WebElement getQuantity() {
+		return this.driver.findElement(By.name("product_qty"));
+
+	}
+
+	public List<WebElement> getProduct() {
+		return this.driver.findElements(By.xpath("//div[@class='product-wrapper']/a"));
+
+	}
+
+	public void addToCart(String quantity) throws InterruptedException {
+		this.getQuantity().clear();
+		this.getQuantity().sendKeys(quantity);
+		Thread.sleep(3000);
+		this.getAddToCartBtn().click();
+	}
 
 }
